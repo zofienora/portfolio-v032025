9738ff33692f298af4f34e58150bb88b67583157
Zofia Nora <sophienora92@gmail.com>
2025-03-11 10:04:51 +0100

    structed components and added them in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css'
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import { useEffect } from "react";
 
 function App() {
+  // Enable smooth scrolling
+  useEffect(() => {
+    document.documentElement.style.scrollBehavior = "smooth";
+  }, []);
 
   return (
     <>
-  
+      <Navbar />
+      <Header />
+      <About />
+      <Projects />
+      <Contact />
     </>
   )
 }
