cb4695010c3d35df25105c7d51d518fdff5646db
Zofia Nora <sophienora92@gmail.com>
2025-03-14 15:07:03 +0100

    did test for scroll behavior, reset to basic content

--- src/components/Letsconnect.jsx
+++ src/components/Letsconnect.jsx
@@ -1,30 +1,14 @@
-import { motion } from "framer-motion";
-
-
 function Letsconnect() {
     return (      
         <div className="letsconnect">
-            <motion.div
-                className="letsconnect-first"
-                initial={{ x: "-100%", opacity: 0 }}  // Moves it further off-screen
-                whileInView={{ x: 0, opacity: 1 }}
-                transition={{ duration: 1, ease: "easeOut" }}
-                viewport={{ once: false, amount: 0.2 }}
-            >
+            <div className="letsconnect-first">
                 Let's Connect // Let's Connect //
-            </motion.div>
-
-            <motion.div
-                className="letsconnect-second"
-                initial={{ x: "100%", opacity: 0 }}
-                whileInView={{ x: 0, opacity: 1 }}
-                transition={{ duration: 0.8, ease: "easeOut" }}
-                viewport={{ once: true }}
-            >
+            </div>
+            <div className="letsconnect-second">
                 Let's Connect // Let's Connect //
-            </motion.div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Letsconnect
+export default Letsconnect;
