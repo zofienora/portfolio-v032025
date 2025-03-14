6bc88f95a625afb6755ae71648e13223cec26d38
Zofia Nora <sophienora92@gmail.com>
2025-03-14 09:11:35 +0100

    added basic content for projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,40 +4,43 @@ function Projects() {
     
     return (
       <>
-      <div className="container">
-        <p className="main_hi">Projects //</p>
-        
-        <div className="pro-img-container">
-            <a className="pro-img-link" href="">
-                <img className="pro-img" src="" alt="" />
-            </a>
-            <div className="project-block">
-                <a href="project">Lorem Project One</a>
-                <div className="project-num"></div>
+      <div className="container project-container">
+        <div className="project-subcontainer">
+            <p className="main_hi">Projects //</p>
+            
+            <div className="pro-img-container">
+                <a className="pro-img-link" href="">
+                    <img className="pro-img" src="" alt="" />
+                </a>
+                <div className="project-block">
+                    <a href="project">Daily To-Dos</a>
+                    <div className="project-num">01</div>
+                </div>
+                <div className="seperator"></div>
             </div>
-            <div className="seperator">__</div>
-        </div>
 
-        <div className="pro-img-container">
-            <a className="pro-img-link" href="">
-                <img className="pro-img" src="" alt="" />
-            </a>
-            <div className="project-block">
-                <a href="project">Lorem Project Two</a>
-                <div className="project-num"></div>
+            <div className="pro-img-container">
+                <a className="pro-img-link" href="">
+                    <img className="pro-img" src="" alt="" />
+                </a>
+                <div className="project-block">
+                    <a href="project">Weather App</a>
+                    <div className="project-num">02</div>
+                </div>
+                <div className="seperator"></div>
             </div>
-            <div className="seperator">__</div>
-        </div>
 
-        <div className="pro-img-container">
-            <a className="pro-img-link" href="">
-                <img className="pro-img" src="" alt="" />
-            </a>
-            <div className="project-block">
-                <a href="project">Lorem Project Three</a>
-                <div className="project-num"></div>
+            <div className="pro-img-container">
+                <a className="pro-img-link" href="">
+                    <img className="pro-img" src="" alt="" />
+                </a>
+                <div className="project-block">
+                    <a href="project">Calculator</a>
+                    <div className="project-num">03</div>
+                </div>
+                <div className="seperator"></div>
             </div>
-            <div className="seperator">__</div>
+            
         </div>
       </div>
       
