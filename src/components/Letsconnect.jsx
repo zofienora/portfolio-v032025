fddb6f1d9bdb23c58fe73a2ca86027cf9517662f
Zofia Nora <sophienora92@gmail.com>
2025-03-15 10:35:58 +0100

    trying scroll behavior for lets connect

--- src/components/Letsconnect.jsx
+++ src/components/Letsconnect.jsx
@@ -1,12 +1,33 @@
+import { motion, useScroll, useMotionValueEvent, useTransform } from "framer-motion";
+import { useState } from "react";
+
+
 function Letsconnect() {
+    const { scrollYProgress } = useScroll(); // Tracks vertical scroll progress
+    useMotionValueEvent(scrollYProgress, "change", (val) => {
+        console.log(val);
+    })
+
+  /*   const scale = useTransform(scrollYProgress, [0.65, 0.85], [0.5, 1]); */
+
+    // Moves from -100% (offscreen left) to 0% (fully visible)
+    const xTransform = useTransform(scrollYProgress, [0.3, 0.6], ["-100%", "0%"]);
+
     return (      
         <div className="letsconnect">
-            <div className="letsconnect-first">
-                Let's Connect // Let's Connect //
-            </div>
-            <div className="letsconnect-second">
-                Let's Connect // Let's Connect //
-            </div>
+            <motion.div
+                style={{ xTransform }}
+                className="letsconnect-first"
+            >
+                Let's Connect //
+            </motion.div>
+
+            <motion.div
+                style={{ xTransform }}
+                className="letsconnect-second"
+            >
+                // Let's Connect
+            </motion.div>
         </div>
     );
 }
