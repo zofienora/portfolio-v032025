7b057a1c00bd7c0ae69cfe3f341e24618d76ab88
Zofia Nora <sophienora92@gmail.com>
2025-03-15 12:44:05 +0100

    added scroll behavoir for seperator in projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,15 @@
+import { motion, useScroll, useMotionValueEvent, useTransform } from "framer-motion";
 
 
 function Projects() {
+    const { scrollYProgress } = useScroll(); // Tracks vertical scroll progress
+    useMotionValueEvent(scrollYProgress, "change", (val) => {
+        console.log(val);
+    })  
+
+    const scale = useTransform(scrollYProgress, [0.5, 0.6], [0.1, 1]);
+    const scalet = useTransform(scrollYProgress, [0.5, 0.7], [0.1, 1]);
+    const scaleh = useTransform(scrollYProgress, [0.5, 0.8], [0.1, 1]);
     
     return (
       <>
@@ -16,7 +25,10 @@ function Projects() {
                     <a href="project">Daily To-Dos</a>
                     <div className="project-num">01</div>
                 </div>
-                <div className="seperator"></div>
+                <motion.div 
+                    style={{ scale: scale }}
+                    className="seperator">
+                </motion.div>
             </div>
 
             <div className="pro-img-container">
@@ -27,7 +39,10 @@ function Projects() {
                     <a href="project">Weather App</a>
                     <div className="project-num">02</div>
                 </div>
-                <div className="seperator"></div>
+                <motion.div 
+                    style={{ scale: scalet }}
+                    className="seperator">
+                </motion.div>
             </div>
 
             <div className="pro-img-container">
@@ -38,7 +53,10 @@ function Projects() {
                     <a href="project">Calculator</a>
                     <div className="project-num">03</div>
                 </div>
-                <div className="seperator"></div>
+                <motion.div 
+                    style={{ scale: scaleh }}
+                    className="seperator">
+                </motion.div>
             </div>
             
         </div>
