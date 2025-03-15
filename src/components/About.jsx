cfdff51fc24f39caba44578ddf1e617a61e6a58c
Zofia Nora <sophienora92@gmail.com>
2025-03-15 13:04:56 +0100

    added framer motion to about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,22 @@
-
+import { motion } from "framer-motion";
 
 function About() {
     
     return (
       <>
         <div className="container about">
-          <div className="about-container">
+          <motion.div 
+            className="about-container"
+            initial={{ opacity: 0, scale: 0.75 }}
+            whileInView={{
+              opacity: 1,
+              scale: 1,
+              transition: {
+                duration: 1
+              }
+            }}
+          
+          >
               <p className="main_hi">About //</p>
               <h2 className="about-description">
                   Hi, I’m Nori, an aspiring frontend developer passionate about crafting sleek, 
@@ -14,7 +25,7 @@ function About() {
                   Beyond coding, city walks inspire me, airports and planes fascinate me, and the gym & water keep me 
                   balanced. I believe creativity comes from both movement and focus.
               </h2>
-          </div>
+          </motion.div>
         </div>
       </>
     )
