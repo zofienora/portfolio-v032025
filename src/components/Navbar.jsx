7e8069b1c28acc1a051e77d057d9e1cf398f6e2f
Zofia Nora <sophienora92@gmail.com>
2025-03-17 20:19:26 +0100

    added menu icon plus useState for hover

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,27 @@
-
+import { useState } from "react";
+import menuWhite from "../assets/menu-white.png";
+import menuGrey from "../assets/menu-grey.png";
 
 function Navbar() {
+  const [menuLogo, setMenuLogo] = useState(menuWhite);
     
     return (
+      
+
+
       <>
       <div className="menubar">
-        
+        <img 
+        src={menuLogo}
+        onMouseEnter={() => {
+          setMenuLogo(menuGrey);
+        }}
+        onMouseLeave={() => {
+          setMenuLogo(menuWhite);
+        }}
+        className="menu-logo"
+        alt="menu" />
+
         
      
 
