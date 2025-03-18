1a65b83744866f4371289f696ca1064a84d862c2
Zofia Nora <sophienora92@gmail.com>
2025-03-18 10:11:13 +0100

    added slide in navbar with links

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,7 +6,27 @@ function Menu() {
     
     return (
       <>
-      
+      <div className="navbarSlide">
+        <img 
+            className="close" 
+            src="" 
+            alt="" 
+        
+        />
+
+
+
+
+
+
+        <ul className="navList">
+            <li className="listItems"><a href="#" className="linkItems">Home</a></li>
+            <li className="listItems"><a href="#" className="linkItems">About</a></li>
+            <li className="listItems"><a href="#" className="linkItems">Projects</a></li>
+            <li className="listItems"><a href="#" className="linkItems">Contact</a></li>
+            <li className="listItems"><a href="#" className="linkItems">Code Laboratory</a></li>
+        </ul>
+      </div>
       </>
     )
   }
