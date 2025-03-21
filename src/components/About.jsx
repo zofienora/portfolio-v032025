928cf53a2957e0b68037c418c734dcbd49d0d5f5
Zofia Nora <sophienora92@gmail.com>
2025-03-21 09:48:41 +0100

    changes about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,11 +19,17 @@ function About() {
           >
               <p className="main_hi">About //</p>
               <h2 className="about-description">
-                  Hi, I’m Nori, an aspiring frontend developer passionate about crafting sleek, 
-                  user-friendly web experiences. I’m committed to lifelong learning, with certifications 
-                  that showcase my dedication to growth and skill development. Always open to new learning experiences.
-                  Beyond coding, city walks inspire me, airports and planes fascinate me, and the gym & water keep me 
-                  balanced. I believe creativity comes from both movement and focus.
+                  I’m Nori, a frontend developer passionate about creating sleek, 
+                  user-friendly web experiences. I love turning ideas into intuitive 
+                  interfaces and continuously expanding my skills to stay ahead in modern web 
+                  development. With certifications backing my growth, I’m always learning and 
+                  refining my craft. I thrive in collaboration, enjoying the process of building and 
+                  problem-solving with others.<br></br><br></br>
+
+                  I get inspired by city walks—observing architecture, soaking in design, and 
+                  letting music set the mood. 
+                  Airports fascinate me—the movement, the energy, the sense of possibility. 
+                  Outside of coding, I lift weights, and I’m always drawn to water. It moves, it rests—just like creativity.
               </h2>
           </motion.div>
         </div>
